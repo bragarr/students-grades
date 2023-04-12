74a73a496ea86a8da26ead2012167ebe8749a93e
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-04-11 23:55:34 -0400

    feat: new authentication process

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Devise reset password config
+
+  config.action_mailer.default_url_options = { host: ENV['APPLICATION_SERVER'] }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['USERS_MAIL_MANAGEMENT'],
+    password: ENV['USERS_MAIL_MANAGEMENT_PASSWORD'],
+    authentication: 'login',
+    tls: false,
+    enable_starttls_auto: true,
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_165257) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_035109) do
   create_table "students", charset: "latin1", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -32,6 +32,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_165257) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
